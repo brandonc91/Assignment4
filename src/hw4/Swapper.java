7d1ccbac7273ede368027e62d08bbc4e8e315d65
Brandon Cruz <brandonc91@gmail.com>
2014-03-11 21:55:50 -0700

    Completed FCFS, no testing done
    FCFS compiles without errors. This was taken almost directly from
    Assignment2.

--- src/hw4/Swapper.java
+++ src/hw4/Swapper.java
@@ -5,6 +5,7 @@ import java.util.*;
 public class Swapper {
 
 	private static Process cpu;
+	public final int MEMSIZE = 100;
 
 	public Swapper() {
 		cpu = new Process();	
@@ -12,16 +13,39 @@ public class Swapper {
 	
 	public static int[] fcfs() {
 		List<HashMap<String, Number>> processLists = cpu.processes;
-		int[] result = new int[102];
 		printList(processLists);
 		HashMap<String, Number> currentProcess = null;
+		int[] result = new int[102];
 		
 		for (int timeCount = 0; timeCount <= 100; timeCount++) {
 			for (int i = 0; i < processLists.size(); i++) {
-				
+				if (processLists.get(i).get("arrivalTime").floatValue() <= timeCount && processLists.get(i).get("completed").floatValue() == 0) {
+					if (currentProcess == null) {
+						currentProcess = processLists.get(i);
+					} 
+				}
 			}
+			int currentCompleted = currentProcess.get("completed").intValue();
+			currentCompleted++;
+			currentProcess.put("completed", currentCompleted);
+			for (int i = 0; i < processLists.size(); i++) {
+				if (processLists.get(i).get("pid").equals(currentProcess.get("pid"))) {
+					processLists.set(i, currentProcess);
+				}
+			}
+			
+			System.out.print(timeCount + "\t");
+			System.out.print("pid: " + currentProcess.get("pid") + "\t");
+			result[timeCount] = currentProcess.get("pid").intValue();
+			System.out.println();
+			
+			
+			if (currentProcess.get("runTime").equals(currentProcess.get("completed"))) {
+				currentProcess = null;
+			}
+			
 		}
-		
+		result[101] = processLists.size();
 		return result;
 	}
 	
@@ -36,4 +60,28 @@ public class Swapper {
 		System.out.println();
 	}
 
+	
+	
+
+	public void firstFit() {
+		// keep list of free blocks of memory
+		// when receiving request for memory, scan list for first block that is large enough to satisfy request
+		// if the block is significantly larger than requested, split block, add remainder to list as a free block
+
+	}	
+
+	public void nextFit() {
+
+	}
+
+	public void bestFit() {
+
+	}
+
+
+	// Run each algorithm 5 times simulating 1 minute each time to get an avg of the # of processes 
+	// successfully swapped into memory
+	
+	
+	
 }
